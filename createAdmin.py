0541a2c212b02105ec7b82d2fdd89262383b598b
belhassenessid <essidbelhassan@gmail.com>
2020-05-16 21:20:25 +0100

    belha

--- createAdmin.py
+++ createAdmin.py
@@ -1,28 +1,43 @@
 from tkinter import *
 import csv
 
-
-def Unic():
-    return True
+def register():
+    username_info=name.get()
+    password_info=password.get()
+    f = open("Administrators.CSV", "w")
+    writing = csv.DictWriter(f, fieldnames=['AdminName', 'AdminPassword'], delimiter=',')
+    writing.writeheader()
+    writing.writerow({'AdminName': username_info, 'AdminPassword': password_info})
+    f.close()
+    enteredName.delete(0,END)
+    enteredPassword.delete(0,END)
+    Label(adminSignupScreen,text='registration success')
 
 def createAdmin():
-    adminScreen = Toplevel(adminScreen)
-    adminScreen.geometry('512x340')
-    adminScreen.title('Administrator Sign up')
-    LabelFrame(adminScreen, text='Use a new username (length>0) and password (length>3)', bg='grey', font=('veranda', 10), height="50",
-               width="2000", ).pack(anchor=CENTER)
+    global adminSignupScreen
+    adminSignupScreen = Tk()
+    adminSignupScreen.geometry('335x125')
+    adminSignupScreen.title('Administrator Sign up')
+    frame = LabelFrame(adminSignupScreen, text='Use a new username (length>0) and password (length>3)')
+    frame.grid(row=1, column=1, columnspan=10, rowspan=10)
 
     # creation
+    global name
+    global password
+    global enteredName
+    global enteredPassword
 
-    Button(adminScreen, text='Create an Account', bd=3, relief='raised', font=("system", 10), height="2", width="30",
-           command=Unic).pack(pady=12, anchor=CENTER)
-    try:
-        f=open("Administrators.CSV",'r+')
-    except:
-        f.close()
-        f = open("Administrators.CSV", "w+")
-        writing = csv.DictWriter(f, fieldnames=['AdminName', 'AdminPassword'], delimiter=',')
-        writing.writeheader()
-        writing.writerow({'AdminName': name, 'AdminPassword': password})
-        f.close()
-    else:
+    name=StringVar()
+    password=StringVar()
+
+    Label(frame, text=' Username').grid(row=2, column=1, sticky=W,pady=3)
+    enteredName = Entry(frame)
+    enteredName.grid(row=2, column=2,pady=3)
+
+    Label(frame, text=' Password').grid(row=5, column=1, sticky=W)
+    enteredPassword = Entry(frame, show='*')
+    enteredPassword.grid(row=5, column=2)
+
+    Label(frame,text='Use this data to create your account').grid(row=6,column=1,sticky=W)
+    Button(frame, text='Create', bd=1, relief='raised', font=("system", 5),width="6",
+           command=register).grid(row=6,column=2,pady=12)
