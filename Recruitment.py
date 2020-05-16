970d97ce0b5cd83bc27edaa204c1e67f95d78c31
belhassenessid <essidbelhassan@gmail.com>
2020-05-16 19:04:31 +0100

    belha

--- Recruitment.py
+++ Recruitment.py
@@ -1,15 +1,32 @@
 from tkinter import *
 
+def AdminOptions():
+
+
 def AdminWindow():
-    screen1=Toplevel(mainScreen)
-    screen1.geometry('1024x681')
-    screen1.title('Administrator Login')
-    
+    # Login Screen
+    global adminScreen
+    adminScreen=Toplevel(mainScreen)
+    adminScreen.geometry('1024x681')
+    adminScreen.title('Administrator Login')
+    LabelFrame(adminScreen,text='Hello Administrator',bg='grey',font=('veranda',30),height="100", width="2000",).pack(anchor=CENTER)
+    # Admin or job seeker ?
+    Button(adminScreen,text='Sign in', bd=3, relief='raised', font=("system", 10), height="2", width="30",
+           command=AdminOptions).pack(pady=12, anchor=CENTER)
+    Button(adminScreen,text='Sign up', bd=3, relief='raised', font=('system', 10), height="2", width="30",
+           command=UserWindow).pack(pady=12, anchor=CENTER)
 
 def UserWindow():
-    screen1 = Toplevel(mainScreen)
-    screen1.geometry('1024x681')
-    screen1.title('User Login')
+    # Login Screen
+    global userScreen
+    userScreen = Toplevel(mainScreen)
+    userScreen.geometry('1024x681')
+    userScreen.title('User Login')
+    # Sign in or Sign up
+    Button(userScreen,text='Sign in', bd=3, relief='raised', font=("system", 10), height="2", width="30",
+           command=AdminWindow).pack(pady=12, anchor=CENTER)
+    Button(userScreen,text='Sign up', bd=3, relief='raised', font=('system', 10), height="2", width="30", command=UserWindow).pack(
+        pady=12, anchor=CENTER)
 
 def main():
     global mainScreen
@@ -24,6 +41,7 @@ def main():
     # Admin or job seeker ?
     Button(text='Administrator',bd=3,relief='raised',font=("system", 10),height="2",width="30",command=AdminWindow).pack(pady=12,anchor=CENTER)
     Button(text='Job Seeker',bd=3,relief='raised',font=('system',10),height="2",width="30",command=UserWindow).pack(pady=12,anchor=CENTER)
-    # Sing in in Sign up page
+
     mainScreen.mainloop()
+
 main()
