ed649be70ca09803d0b2bc3b9588eb156f4e19ad
belhassenessid <essidbelhassan@gmail.com>
2020-05-16 18:47:29 +0100

    belha

--- Recruitment.py
+++ Recruitment.py
@@ -1,6 +1,17 @@
 from tkinter import *
 
+def AdminWindow():
+    screen1=Toplevel(mainScreen)
+    screen1.geometry('1024x681')
+    screen1.title('Administrator Login')
+
+def UserWindow():
+    screen1 = Toplevel(mainScreen)
+    screen1.geometry('1024x681')
+    screen1.title('User Login')
+
 def main():
+    global mainScreen
     mainScreen=Tk()
     mainScreen.geometry('1280x852')
     mainScreen.title('Recruitment')
@@ -10,8 +21,8 @@ def main():
     bg_label = Label(mainScreen, image=bg_image)
     bg_label.place(x=0, y=0, relwidth=1, relheight=1)
     # Admin or job seeker ?
-    Button(text='Administrator',bd=3,relief='raised',font=("system", 10)).pack(pady=12)
-    Button(text='Job Seeker',bd=3,relief='raised',font=('system',10)).pack(pady=12)
+    Button(text='Administrator',bd=3,relief='raised',font=("system", 10),height="2",width="30",command=AdminWindow).pack(pady=12,anchor=CENTER)
+    Button(text='Job Seeker',bd=3,relief='raised',font=('system',10),height="2",width="30",command=UserWindow).pack(pady=12,anchor=CENTER)
     # Sing in in Sign up page
     mainScreen.mainloop()
 main()
