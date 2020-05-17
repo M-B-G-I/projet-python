c34aa65f2924ebbef59f09691c0e3560f309aace
belhassenessid <essidbelhassan@gmail.com>
2020-05-17 01:44:37 +0100

    belha

--- AdminAuthentificationAndOptionsWindow.py
+++ AdminAuthentificationAndOptionsWindow.py
@@ -1,2 +1,54 @@
+from tkinter import *
+import csv
+from tkinter import messagebox
+
+def verif():
+    username_info = enteredName.get()
+    password_info = enteredPassword.get()
+    try:
+        f = open("Administrators.CSV", 'r')
+    except FileNotFoundError:
+        msg = messagebox.showinfo('Error', "Create an account then Retry")
+        adminSigninScree.destroy()
+    else:
+        admins=csv.reader(f,delimiter=',')
+        good=False
+        for i in list(admins)[0:-1:2]:
+            if i[0]==username_info and i[1]==password_info:
+                good=True
+                break
+        f.close()
+        if good==False:
+            msg = messagebox.showinfo('ok', "log in success")
+            adminSigninScree.destroy()
+            # list of options
+        else:
+            enteredName.delete(0, END)
+            enteredPassword.delete(0, END)
+            msg = messagebox.showinfo('Error 403', "Wrong username or password, Retry")
+
+
 def AdminAuthAndOp():
-    f=open()
+    global adminSigninScree
+    adminSigninScreen = Tk()
+    adminSigninScreen.geometry('335x125')
+    adminSigninScreen.title('Administrator Sign in')
+    frame = LabelFrame(adminSigninScreen, text='Enter your Username and your Password')
+    frame.grid(row=1, column=1, columnspan=10, rowspan=10)
+
+    # creation
+    global enteredName
+    global enteredPassword
+
+    Label(frame, text=' Username').grid(row=2, column=1, sticky=W, pady=3)
+    enteredName = Entry(frame)
+    enteredName.grid(row=2, column=2, pady=3)
+
+    Label(frame, text=' Password').grid(row=5, column=1, sticky=W)
+    enteredPassword = Entry(frame, show='*')
+    enteredPassword.grid(row=5, column=2)
+
+    Label(frame, text='Use this data to create your account').grid(row=6, column=1, sticky=W)
+    Button(frame, text='Log in', bd=1, relief='raised', font=("system", 5), width="6", command=verif).grid(row=6,
+                                                                                                              column=2,
+                                                                                                              pady=12)
