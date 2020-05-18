89492090b35217f84708086b9976d0abf23f3c77
ghassen oueslati <oueslatighassen5201@gmail.com>
2020-05-18 01:59:55 +0100

    newnew

--- searchJob.py
+++ searchJob.py
@@ -2,18 +2,22 @@ from tkinter import *
 from tkinter import messagebox
 import csv
 
+def search():
+    key_info=key.get()
+    try:
+        f=open('jobs.csv','r')
+
 def searchWindow():
     global searchJobWindow
+    global key
     searchJobWindow = Tk()
     searchJobWindow.geometry('1024x681')
     searchJobWindow.title('Search for a Job')
-    LabelFrame(searchJobWindow, text='Search for a Job', bg='grey', font=('veranda', 30), height="100",
+    frame=LabelFrame(searchJobWindow, text='Search for a Job', bg='grey', font=('veranda', 30), height="100",
                width="2000", ).pack(anchor=CENTER)
-    Button(searchJobWindow, text='', bd=3, relief='raised', font=("system", 10), height="2", width="30",
-           #command=AdminAuthAndOp
-           ).pack(pady=12, anchor=CENTER)
-    Button(searchJobWindow, text='', bd=3, relief='raised', font=('system', 10), height="2", width="30",
-           #command=createAdmin
-           ).pack(pady=12, anchor=CENTER)
-    searchJobWindow.mainloop()
+    Label(frame,text='Fill in the blank the ID OR the Location OR the Domain of the Job you are searching for').pack()
+    key=Entry(frame)
+    key.pack()
+
+    Button(frame,text='Search',command=search).pack()
 #searchWindow()
